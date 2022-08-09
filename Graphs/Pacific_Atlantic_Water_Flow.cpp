1c365d1b52b27a4fa0467818d94f269349c2d2f2
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-10 01:20:26 +0530

    commit

--- Graphs/Pacific_Atlantic_Water_Flow.cpp
+++ Graphs/Pacific_Atlantic_Water_Flow.cpp
@@ -3,15 +3,22 @@ public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         vector<vector<int>> ans;
         int m=heights.size(),n=heights[0].size();
-        vector<char> dp(m*n);
+        vector<int> dp(m*n);
+        // Initializing the dp 1d array to zero for every cell.
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                dp[i*n+j] = 0;
+
+
         if(m<1) return ans;
+        for
         for(int i=0;i<m;i++){
-            dfs(heights,i,0,dp,m,n,ans);
-            dfs(heights,i,n-1,dp,m,n,ans);
+            dfs(heights,i,0,dp,w,ans);
+            dfs(heights,i,n-1,dp,w,ans);
         }
         for(int i=0;i<n;i++){
-            dfs(heights,0,i,dp,m,n,ans);
-            dfs(heights,m-1,dp,i,m,n,ans);
+            dfs(heights,0,i,dp,w,ans);
+            dfs(heights,m-1,dp,i,w,ans);
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
@@ -22,14 +29,15 @@ public:
         return ans;
     }
     
-    void dfs(vector<vector<int>>& heights,int i,int j,vector<int>& dp,int m,int n,vector<vector<int>>& ans){
+    void dfs(vector<vector<int>>& heights,int i,int j,vector<int>& dp,int w,int h,vector<vector<int>>& ans){
         if(i<0||i>=m||j<0||j>=n||heights[i][j]==0) return;
+        if((dp[ij] & w) || heights[i][j]<h) return;
+        
 
 
-
-        dfs(heights,i+1,j,m,n,ans);
-        dfs(heights,i-1,j,m,n,ans);
-        dfs(heights,i,j+1,m,n,ans);
-        dfs(heights,i,j-1,m,n,ans);
+        dfs(heights,i+1,j,w,ans); // Next Column
+        dfs(heights,i-1,j,w,ans); // Previous Column
+        dfs(heights,i,j+1,w,ans); // Next Row
+        dfs(heights,i,j-1,w,ans); // Previous Row
     }
 };
