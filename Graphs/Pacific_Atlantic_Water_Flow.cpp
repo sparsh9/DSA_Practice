0a41df32dd231227b566e7c359ad79e3916fabff
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-09 22:06:21 +0530

    commit

--- Graphs/Pacific_Atlantic_Water_Flow.cpp
+++ Graphs/Pacific_Atlantic_Water_Flow.cpp
@@ -1,26 +1,35 @@
-// Leetcod problem of Pacific Atlantic Water Flow
-
-vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights){
-    vector<vector<int>> res;
-    int m = heights.size();
-    int n = heights[0].size();
-    vector<vector<int>> pacific(m,vector<int>(n,0));
-    vector<vector<int>> atlantic(m,vector<int>(n,0));
-
-    for(int i=0;i<m;i++){
-        dfs(heights,i,0,pacific);
-        dfs(heights,i,n-1,atlantic);
-    }
-    for(int j=0;j<n;j++){
-        dfs(heights,0,j,pacific);
-        dfs(heights,m-1,j,atlantic);
-    }
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(pacific[i][j]==1 && atlantic[i][j]==1){
-                res.push_back({i,j});
+class Solution {
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        vector<vector<int>> ans;
+        int m=heights.size(),n=heights[0].size();
+        vector<char> dp(m*n);
+        if(m<1) return ans;
+        for(int i=0;i<m;i++){
+            dfs(heights,i,0,m,n,ans);
+            dfs(heights,i,n-1,m,n,ans);
+        }
+        for(int i=0;i<n;i++){
+            dfs(heights,0,i,m,n,ans);
+            dfs(heights,m-1,i,m,n,ans);
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                
             }
         }
+
+        return ans;
     }
-    return res;
-}
+    
+    void dfs(vector<vector<int>>& heights,int i,int j,int m,int n,vector<vector<int>>& ans){
+        if(i<0||i>=m||j<0||j>=n||heights[i][j]==0) return;
+
+
+
+        dfs(heights,i+1,j,m,n,ans);
+        dfs(heights,i-1,j,m,n,ans);
+        dfs(heights,i,j+1,m,n,ans);
+        dfs(heights,i,j-1,m,n,ans);
+    }
+};
