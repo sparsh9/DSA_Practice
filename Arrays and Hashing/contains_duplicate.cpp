d124dcf8d56763c7bbb940a2191053874a17490f
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-18 00:03:18 +0530

    commit

--- Arrays and Hashing/contains_duplicate.cpp
+++ Arrays and Hashing/contains_duplicate.cpp
@@ -3,14 +3,55 @@ using namespace std;
 
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int,int> maps;
-        for(int i=0;i<n;i++){
-            if(maps.find(nums[i])==maps.end()) return true;
-            maps[nums[i]] = 1;
-        }
-        return false;
-    }
 
-};
+    // Approach 1
+    // bool containsDuplicate(vector<int>& nums) {
+    //     // int n = nums.size();
+    //     unordered_map<int,int> maps;
+    //     for(auto &x:nums)
+    //     {
+    //         maps[x]++;
+    //     }
+        
+    //     for(auto &z:maps)
+    //     {
+    //         if(z.second>=2)
+    //             return true;
+    //     }
+        
+    //     return false;
+    // }
+
+    // Approach 2
+    // bool containsDuplicate(vector<int>& nums) {
+    //     set<int> maps;
+		
+    //     maps.insert(nums.begin(),nums.end()); 
+		
+    //     bool ans = nums.size()!=maps.size();  
+		
+    //     return ans;
+    // }
+
+    // Approach 3
+    // bool containsDuplicate(vector<int>& nums) {
+        
+    //     unordered_map<int,int> duplicate;
+    //     int count ;
+
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         duplicate[nums[i]]++;
+    //     }
+    //     for(auto character : duplicate)
+    //     {
+    //         if(character.second == 2 || character.second > 1)
+    //         {
+    //             return true;
+    //         }
+    //     }
+        
+    //     return false;
+    // }
+
+}; 
