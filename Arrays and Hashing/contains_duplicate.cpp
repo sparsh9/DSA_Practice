94c7ead9559d3e967f5770d2577ed6e94cc2f7c0
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-17 23:19:05 +0530

    commit

--- Arrays and Hashing/contains_duplicate.cpp
+++ Arrays and Hashing/contains_duplicate.cpp
@@ -1,14 +1,16 @@
+#include <unordered_map>
+using namespace std;
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> map(n);
+        unordered_map<int,int> maps;
         for(int i=0;i<n;i++){
-            if(map.find(nums[i])){
-                return true;
-            }
-            map[nums[i]] = 1;
+            if(maps.find(nums[i])==maps.end()) return true;
+            maps[nums[i]] = 1;
         }
+        return false;
     }
 
 };
