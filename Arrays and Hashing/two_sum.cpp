47ef91f36380f88add99179c1ae6358ebe6e1a7c
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-18 13:56:54 +0530

    commit

---
+++ Arrays and Hashing/two_sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // Approach 1 using HashMaps
+    // vector<int> twoSum(vector<int>& nums, int target) {
+    //     int l=0,r=nums.size()-1;
+    //     vector<int> ans;
+    //     sort(nums.begin(),nums.end());
+    //     while(l<r){
+    //         int sum = nums[l]+nums[r];
+    //         if(sum == target)
+    //            break; 
+    //         else if (sum > target)
+    //             r--;
+    //         else 
+    //             l++;
+    //     }
+    //     return {l,r};
+    // }
+
+    // Approach 2
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int 
+    }
+
+
+
+
+};
