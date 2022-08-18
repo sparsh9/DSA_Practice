17a270b9bd77254e137b6451f8c3e3ecdc849855
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-18 17:28:11 +0530

    commit

--- Arrays and Hashing/two_sum.cpp
+++ Arrays and Hashing/two_sum.cpp
@@ -2,24 +2,41 @@ class Solution {
 public:
     // Approach 1 using HashMaps
     // vector<int> twoSum(vector<int>& nums, int target) {
-    //     int l=0,r=nums.size()-1;
+    //     unordered_map<int,int> mp;
     //     vector<int> ans;
-    //     sort(nums.begin(),nums.end());
-    //     while(l<r){
-    //         int sum = nums[l]+nums[r];
-    //         if(sum == target)
-    //            break; 
-    //         else if (sum > target)
-    //             r--;
-    //         else 
-    //             l++;
+    //     for(int i=0;i<nums.size();i++){
+    //         int sum = target - nums[i];
+    //         if(mp.find(sum) != mp.end()){
+    //             ans.push_back(mp[sum]);
+    //             ans.push_back(i);
+    //             return ans;
+    //         }
+    //         mp[nums[i]] = i;
     //     }
-    //     return {l,r};
+    //     return ans;
+        
     // }
 
     // Approach 2
     vector<int> twoSum(vector<int>& nums, int target) {
-        int 
+        int l=0,r=nums.size()-1;
+        vector<int> ans;
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]] = i;
+        }
+        sort(nums.begin(),nums.end());
+        while(l<r){
+            int sum = nums[l]+nums[r];
+            if(sum == target){}
+            else if (sum > target)
+                r--;
+            else 
+                l++;
+        }
+        ans.push_back(mp.find(nums[l])->second);
+        ans.push_back(mp.find(nums[r])->second);
+        return ans;
     }
 
 
