8503ec7d010eadf99c10a87b928b2515232b0c4e
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-19 18:04:39 +0530

    commit

---
+++ Leetcode/split_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+bool isPossible(vector<int>& nums) {
+        
+    }
+
+int main()
+{
+    vector<int> nums= [1,2,3,3,4,5];
+    if(isPossible(nums))
+        cout<<"true";
+    else
+        cout<<"false";
+    return 0;
+}
