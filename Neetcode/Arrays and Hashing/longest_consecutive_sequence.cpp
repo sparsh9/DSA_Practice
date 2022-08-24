ce736bb02df07d5f8c0e036db295e69f1544e12f
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-24 22:51:36 +0530

    commit

---
+++ Neetcode/Arrays and Hashing/longest_consecutive_sequence.cpp
@@ -0,0 +1,10 @@
+// Approach 1
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        priority_queue<int>q(nums.size());
+        for(int i=0;i<nums.size();i++){
+            
+        }
+    }
+};
