fc6366a8a8ae5e7258060ec01e24a3489297a663
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-25 02:07:50 +0530

    commit

--- 450_DSA_Sheet/reverse_string.cpp
+++ 450_DSA_Sheet/reverse_string.cpp
@@ -1,16 +1,30 @@
-class Solution {
-public:
-    void reverseString(vector<char>& s) {
-        int left=0 ,right=s.size()-1;
-        for(int i=0;i<s.size();i++){
-            char temp;
-            temp=s[right];
-            s[right]=s[left];
-            s[left]=temp;
-            left++;
-            right--;
-        }
-        for(auto item:s)
-        cout<<item;
-    }
-};
+// Approach 1
+// class Solution {
+// public:
+//     void reverseString(vector<char>& s) {
+//         int left=0 ,right=s.size()-1;
+//         while(left<right){
+//             char temp;
+//             temp=s[right];
+//             s[right]=s[left];
+//             s[left]=temp;
+//             left++;
+//             right--;
+//         }
+//         for(auto item:s)
+//         cout<<item;
+//     }
+// };
+
+// Approach 2
+// class Solution {
+// public:
+//     void reverseString(vector<char>& s) {
+//         int left=0 ,right=s.size()-1;
+//         while(left<right){
+//             swap(s[left++],s[right--]);
+//         }
+//         for(auto item:s)
+//         cout<<item;
+//     }
+// };
