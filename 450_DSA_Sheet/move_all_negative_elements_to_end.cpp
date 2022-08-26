c803e3159257f6c73c9bc39ea774e1b22d37f540
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-26 10:13:06 +0530

    commit

--- 450_DSA_Sheet/move_all_negative_elements_to_end.cpp
+++ 450_DSA_Sheet/move_all_negative_elements_to_end.cpp
@@ -7,10 +7,20 @@ class Solution{
     public:
     void segregateElements(int arr[],int n)
     {
-        int left=0,right=0;
-        while(left<right){
-            if(arr[left])
-        }
+       vector<int>v;
+       for(int i=0;i<n;i++){
+           if(arr[i]>0){
+               v.push_back(arr[i]);
+           }
+       }
+       for(int i=0;i<n;i++){
+           if(arr[i]<0){
+               v.push_back(arr[i]);
+           }
+       }
+       for(int i=0;i<n;i++){
+           arr[i]=v[i];
+       }
     }
 };
 
