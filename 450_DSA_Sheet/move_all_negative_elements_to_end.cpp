78db05f06a2b450200463ae8f68329f83da8e71e
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-26 09:41:22 +0530

    commit

---
+++ 450_DSA_Sheet/move_all_negative_elements_to_end.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    void segregateElements(int arr[],int n)
+    {
+        int left=0,right=0;
+        while(left<right){
+            if(arr[left])
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution ob;
+		ob.segregateElements(a,n);
+		
+        for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+		cout<<endl;
+	}
+}
+
+// } Driver Code Ends
---
+++ 450_DSA_Sheet/sort_colours.cpp
@@ -0,0 +1,27 @@
+// Approach 1 (storing the counts of 0s,1s,2s in the vectors)
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+        
+//     }
+// };
+
+// Approach 2 (using 3-pointer)
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+            switch(nums[mid]){
+                case 0 : swap(nums[mid++],nums[low++]);break;
+                case 1 : mid++;break;
+                case 2 : swap(nums[mid],nums[high--]);break;
+            }
+
+        }
+        for(auto item:nums){
+            cout<<item;
+        }
+    }
+};
