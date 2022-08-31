d3d5f19095e1667ae691a9ca6fe2c411677d9fb3
Prat2030 <pratyushjindal2003@gmail.com>
2022-08-31 09:57:28 +0530

    commit

--- 450_DSA_Sheet/rotate_array.cpp
+++ 450_DSA_Sheet/rotate_array.cpp
@@ -5,7 +5,7 @@ public:
         int n=nums.size(),temp=nums[n-1];
         for(int i=n-1;i>0;i--){
             nums[i]=nums[i-1];
-            num[0]=temp;
+            nums[0]=temp;
         }
     }
     void rotate(vector<int>& nums, int k) {
