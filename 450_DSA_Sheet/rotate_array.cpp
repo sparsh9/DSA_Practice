3d1e17cc37c728507fbccc20bf6d50b441b2ae61
Prat2030 <pratyushjindal2003@gmail.com>
2022-09-20 22:28:50 +0530

    commit

--- 450_DSA_Sheet/rotate_array.cpp
+++ 450_DSA_Sheet/rotate_array.cpp
@@ -1,17 +1,26 @@
-// Approach (using rotate one by one)
+
+// Approach (Mathematical and logical)
 class Solution {
 public:
-    void rotateByOne(vector<int>& nums){
-        int n=nums.size(),temp=nums[n-1];
-        for(int i=n-1;i>0;i--){
-            nums[i]=nums[i-1];
-            nums[0]=temp;
-        }
-    }
     void rotate(vector<int>& nums, int k) {
-         while(k>0){
-            rotateByOne(nums);
-            k--;
+             
+        //in-place 
+        k = k%nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+    }
+};
+
+// Approach (Using extra space)
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> temp(n);
+        for(int i=0; i<n; i++){
+            temp[(i+k)%n] = nums[i];
         }
+        nums = temp;
     }
 };
