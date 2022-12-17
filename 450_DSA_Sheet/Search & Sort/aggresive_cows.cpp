710226886f54e418087f65edb3ab1d1f66341f3c
Prat2030 <pratyushjindal2003@gmail.com>
2022-12-18 00:58:04 +0530

    commit

--- 450_DSA_Sheet/Search & Sort/aggresive_cows.cpp
+++ 450_DSA_Sheet/Search & Sort/aggresive_cows.cpp
@@ -1,11 +1,12 @@
+// In this function we are checking the cow placements and possibility.
 bool isPossible (vector<int>& stalls,int k,int mid){
     int n=stalls.size();
-    int cnt=1;
-    int last_cow_loc=stalls[0];
+    int cnt=1; // as we have already placed a cow
+    int last_cow_loc=stalls[0]; // initially placed the first cow at 1st position
     for(int i=0;i<n;i++){
         if(stalls[i]-last_cow_loc >= mid){
             cnt++;
-            last_cow_loc = stalls[i];
+            last_cow_loc = stalls[i]; // to place next cow
             if(cnt >= k)
                 return true;
         }
@@ -21,6 +22,9 @@ int aggressiveCows(vector<int> &stalls, int k)
     int ans=-1;
     int mid = s+(e-s)/2;
     while(s<=e){
+        // if suppose all cows can't be placed at max distance of 3 then 
+        // it is obvious that cows can't be placed at distances more than 3 and 
+        // due to this we have a search space and therefore we apply the concept of binary search.
         if(isPossible(stalls,k,mid)){
             ans = mid;
             s=mid+1;
