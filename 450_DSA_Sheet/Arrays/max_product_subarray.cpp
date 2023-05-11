1f00eb11766c0ee050a97b475628869f1f1175bf
Prat2030 <pratyushjindal2003@gmail.com>
2023-05-11 23:58:37 +0530

    commit

--- 450_DSA_Sheet/Arrays/max_product_subarray.cpp
+++ 450_DSA_Sheet/Arrays/max_product_subarray.cpp
@@ -1,8 +1,34 @@
+
+// Approach 1 using Kadane's Algorithm
+// we will check from both left -> right and right -> left incase 
+// if there may be a 0 in between the sequence, then we make the max_pro as 1
+// and again start from next subarray after 0
+// Also we are taking from both sides as if there are odd numbers of -ve numbers
+// then in one of the cases the odd'th -ve number will be left in max_pro from either side.
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int max_pro_current = 0;
-        int max_pro_till = 0;
+        int max_pro_current = INT_MIN;
+        int max_pro = 1;
+        // checking left to right
+        for (int i=0;i<nums.size();i++){
+            max_pro *= nums[i];
+            max_pro_current = max(max_pro,max_pro_current);
+            if(max_pro == 0)
+                max_pro = 1;
+        }
+        // Till here we have checked 
+        max_pro = 1;
+        for(int i = nums.size()-1;i>=0;i--){
+            max_pro *= nums[i];
+            max_pro_current = max(max_pro,max_pro_current);
+            if(max_pro ==0)
+                max_pro = 1;
+        }
+        return max_pro;
         
     }
 };
+
+
+// Approach 2 using Dynamic Programming
---
+++ 450_DSA_Sheet/Arrays/subarray_sum_equals_k.cpp
@@ -0,0 +1,17 @@
+
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int cnt =0,preSum =0;
+        unordered_map<int,int> map;
+        map[0] = 1;
+        for(int i=0;i<nums.size();i++){
+            preSum += nums[i];
+            int preSum_sub = preSum - k;
+            cnt += map[preSum_sub];
+            map[preSum] += 1;
+        }
+        return cnt;
+    }
+};
