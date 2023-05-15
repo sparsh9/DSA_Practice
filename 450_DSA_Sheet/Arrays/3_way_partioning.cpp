72415e4ab962e6398e165dc2424728303d190e3c
Prat2030 <pratyushjindal2003@gmail.com>
2023-05-15 13:59:43 +0530

    commit

--- 450_DSA_Sheet/Arrays/3_way_partioning.cpp
+++ 450_DSA_Sheet/Arrays/3_way_partioning.cpp
@@ -14,7 +14,13 @@ public:
     //that array is divided into three parts.
     void threeWayPartition(vector<int>& array,int a, int b)
     {
-        // code here 
+        int l=0,r=array.size()-1;
+        for(int i=0;i<=r;i++){
+            if(array[i]<a)
+                swap(array[i],array[l++]);
+            else if(array[i]>b)
+                swap(array[i++],array[r--]);
+        }
     }
 };
 
