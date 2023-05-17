7c4fc6c00ee21a6ef0316e759dbb8966dc3bbd4e
Prat2030 <pratyushjindal2003@gmail.com>
2023-05-17 16:34:37 +0530

    commit

---
+++ 450_DSA_Sheet/Strings/remove_consecutive_characters.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string S)
+    {
+        int size = S.size();
+        string ans(size,'x');
+        for(int i=0;i<size;i++){
+            if(S[i] != S[i+1])
+                ans[i] = S[i];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.removeConsecutiveCharacter(s)<<endl;
+    }
+} 
+
+
+
+
+// } Driver Code Ends
