02004f4e7cfe87d15d9e4a2cd48513ecac3de67b
Prat2030 <pratyushjindal2003@gmail.com>
2023-05-17 21:56:32 +0530

    commit

---
+++ 450_DSA_Sheet/Strings/minimum_number_of_swaps_for_bracket_balancing.cpp
@@ -0,0 +1,56 @@
+// Approach 1 with O(n) space and O(n) time complexity
+class Solution{   
+public:
+    int minimumNumberOfSwaps(string s){
+        vector<int> v;
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '[')
+                v.push_back(i);
+        } 
+        int idx=0; // the index of the '[' in the array to balance the imbalance by ']'
+        int ans = 0; // counts the number of swaps
+        int count = 0; // counts the balance of the string array
+
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '['){
+                idx++;
+                count++;
+            }
+            else{
+                count--;
+                if(count<0){
+                    ans += v[idx]-i; // gets the total number of swaps needed to switch
+                    swap(s[i],s[v[idx]]);
+                    count = 1;
+                    idx++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// Approach 2 More optimal to the above without using the extra space
+class Solution{   
+public:
+    int minimumNumberOfSwaps(string s){
+        int ans = 0;
+        int open=0,close=0;
+        int fault=0;
+        for(int i=0;i<=s.size();i++){
+            if(s[i] == ']'){
+                close++;
+                fault = close-open;
+            }
+            else{
+                open++;
+                if(fault>0){
+                    ans += fault;
+                    fault--;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
