dffeb4f5c7bba5436ebcb784c4b7209053fc82e6
Prat2030 <pratyushjindal2003@gmail.com>
2023-05-19 01:14:56 +0530

    commit

---
+++ 450_DSA_Sheet/Strings/valid_parenthesis.cpp
@@ -0,0 +1,26 @@
+// Approach 1 using stacks and some logic
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stk.push(c);
+            } else {
+                if (stk.empty()) {
+                    return false;
+                }
+                if (c == ')' && stk.top() == '(') {
+                    stk.pop();
+                } else if (c == '}' && stk.top() == '{') {
+                    stk.pop();
+                } else if (c == ']' && stk.top() == '[') {
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stk.empty();
+    }
+};
