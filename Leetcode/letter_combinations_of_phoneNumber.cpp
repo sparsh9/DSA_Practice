eb9b484b6f17a6e9702e0dc0603bb0e691613b6e
Prat2030 <pratyushjindal2003@gmail.com>
2023-05-21 23:04:39 +0530

    commit

---
+++ Leetcode/letter_combinations_of_phoneNumber.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    void solve(string digit, string output, int index, vector<string>& ans, string mapping[] ) {
+        
+        //base case (when pointer goes out of the index)
+        if(index >= digit.length()) {
+            ans.push_back(output);
+            return;
+        }
+        
+        int number = digit[index] - '0';
+        string value = mapping[number];
+        
+        for(int i=0; i<value.length(); i++) {
+            output.push_back(value[i]); // Here we are appending to the empty string after every recursion 
+            // example , "" -> "a" -> "ad" after every recursion
+            solve(digit, output, index+1, ans, mapping);
+            output.pop_back();
+        }
+        
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.length()==0)
+            return ans;
+        string output;
+        int index = 0;
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits, output, index, ans, mapping);
+        return ans;
+    }
+};
