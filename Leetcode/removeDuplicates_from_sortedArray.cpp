f160be3327ff7aafaeee6a5811ea07bfd2d54191
Prat2030 <pratyushjindal2003@gmail.com>
2023-05-22 01:03:44 +0530

    commit

---
+++ Leetcode/removeDuplicates_from_sortedArray.cpp
@@ -0,0 +1,19 @@
+// Approach 1 using 2 pointers
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n= nums.size();
+        if(n<2)
+            return n;
+        // Initially there will be atleast 1 unique element hence ans = 1 
+        int ans=1, left=0;
+        for(int right=1; right<n; right++){
+            if(nums[right]!=nums[left]){
+                left++;
+                nums[left]=nums[right];
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
