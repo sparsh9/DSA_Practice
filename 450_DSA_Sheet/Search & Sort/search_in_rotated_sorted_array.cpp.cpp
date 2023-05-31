0dc2f2ee6630dcdc36932ecd46b9f12f5b25c2a9
Prat2030 <pratyushjindal2003@gmail.com>
2023-06-01 00:34:40 +0530

    commit

--- 450_DSA_Sheet/Search & Sort/search_in_rotated_sorted_array.cpp.cpp
+++ 450_DSA_Sheet/Search & Sort/search_in_rotated_sorted_array.cpp.cpp
@@ -40,4 +40,37 @@ public:
 };
 
 // Method 2
-
+// Given Array must be sorted on either side of the nums[mid], therefore we are checking for both the cases where it is
+// when we get the side where it is sorted
+// then we check if the target lies in that region or not
+// If the target lies in that region, then eventually after iterations, its mid will come to the targets index and then we will return it
+// Otherwise if the target does not lie in that region, we will immediately switch to other side, even though if it is not sorted
+// If neither cases follow or if after several iterations, the mid does not reach target's idex, we simply return -1.
+// This way we are reducing our search space using the binary search
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+     int n = nums.size();
+      int left = 0;
+      int right = n-1;
+      int mid= left + (right - left) / 2;
+      while(left <= right){
+        if(nums[mid] == target)
+         return mid;
+        if(nums[mid] >= nums[left]) {
+          if(target >= nums[left] && target <= nums[mid])
+            {
+                right = mid - 1;
+            }
+           else left = mid + 1;
+        } 
+        else {
+          if(target >= nums[mid] && target <= nums[right]) 
+            left = mid + 1;
+          else right = mid - 1;
+        }
+          mid = left + (right - left) / 2;
+      }
+      return -1;
+    }
+};
