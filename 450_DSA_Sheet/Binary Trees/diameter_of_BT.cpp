6f51b69335b7ece1a0d7d8c7a036dcbfd6a89405
Prat2030 <pratyushjindal2003@gmail.com>
2023-06-22 01:16:01 +0530

    commit

---
+++ 450_DSA_Sheet/Binary Trees/diameter_of_BT.cpp
@@ -0,0 +1,26 @@
+// Approach (using height of tree method)
+// Here just we are calculating the total height (lh+rh) for every node 
+// and maximising our diamter variable using the max function every time and every stage
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
+    }
+
+private:
+    int height(TreeNode* node, int& diameter) {
+
+        if (!node) {
+            return 0;
+        }
+
+        int lh = height(node->left, diameter);
+        int rh = height(node->right, diameter);
+
+        diameter = max(diameter, lh + rh);
+
+        return 1 + max(lh, rh);
+    }
+};
