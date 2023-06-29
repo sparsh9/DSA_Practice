8df72386d9141f6f5921aa30ab48fb229998a62c
Prat2030 <pratyushjindal2003@gmail.com>
2023-06-29 22:30:46 +0530

    commit

---
+++ 450_DSA_Sheet/Binary Trees/path_sum_3.cpp
@@ -0,0 +1,23 @@
+// Approach 1(Using Recursion)
+class Solution {
+public:
+    int ans=0;
+    int pathSum(TreeNode* root, int sum) {
+        if(root){
+            // checking from this node to others
+            dfs(root,sum);
+            // checking on from nodes on left side
+            pathSum(root->left,sum);
+            // checking on from nodes on right side
+            pathSum(root->right,sum);
+        }
+        return ans;
+    }
+    // function to check if there is any path which equals target sum
+    void dfs(TreeNode* root, long long int sum){
+        if(!root)return;
+        if(root->val==sum)ans++;
+        dfs(root->left,sum-root->val);
+        dfs(root->right,sum-root->val);
+    }
+};
