d4e7aea34da63fbecf68b0b7e3e7ac4da6335e3c
Prat2030 <pratyushjindal2003@gmail.com>
2023-07-27 00:28:09 +0530

    commit

---
+++ 450_DSA_Sheet/Dynamic Programming/coin_change_2.cpp
@@ -0,0 +1,84 @@
+// Approach (Using Memoization) (Bottom Up)
+class Solution {
+public:
+    int helper(vector<int>& coins, int ind, int amount, vector<vector<int>>& dp) {
+        // Base Case
+        if(amount == 0) return 1;
+
+        if(ind==0){
+            return (amount % coins[ind] == 0);
+        }
+
+        // case of take and not take
+        long not_take = helper(coins, ind-1, amount, dp);
+        long take = 0;
+        if(amount >= coins[ind]){
+            take = helper(coins, ind, amount-coins[ind], dp);
+        }
+        return dp[ind][amount] = not_take + take;
+        
+    }
+
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+
+        return helper(coins, n-1, amount, dp);
+    }
+};
+
+
+// Approach (Using Tabulation) Top Down
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, 0));
+
+        // Base Case
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=amount; j++){
+                // case of take and not take
+                long not_take = dp[i-1][j];
+                long take = 0;
+                if(j >= coins[i-1]){
+                    take = dp[i][j-coins[i-1]];
+                }
+                dp[i][j] = not_take + take;
+            }
+        }
+        return dp[n][amount];
+    }
+};
+
+
+
+
+// Approach (Tabulation Space Optimized)
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<int> dp(amount+1, 0);
+
+        // Base Case
+        dp[0] = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=amount; j++){
+                // case of take and not take
+                long not_take = dp[j];
+                long take = 0;
+                if(j >= coins[i-1]){
+                    take = dp[j-coins[i-1]];
+                }
+                dp[j] = not_take + take;
+            }
+        }
+        return dp[amount];
+    }
+};
--- 450_DSA_Sheet/Search & Sort/search_in_rotated_sorted_array.cpp.cpp
+++ 450_DSA_Sheet/Search & Sort/search_in_rotated_sorted_array.cpp.cpp
@@ -1,43 +1,56 @@
-// Method 1
-
+// Method 1 (Brute Force)
 class Solution {
-    int findPivot(vector<int>& nums){
-        int s=0,e=nums.size()-1;
-        int mid = s+(e-s)/2;
-        while(s<e){
-            if(nums[mid]>=nums[0])
-                s=mid+1;
+    // Function to find the pivot element in the rotated sorted array
+    int findPivot(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = start + (end - start) / 2;
+
+        while (start < end) {
+            if (nums[mid] >= nums[0])
+                start = mid + 1;
             else
-                e=mid;
-            
-        mid = s+(e-s)/2;
+                end = mid;
+
+            mid = start + (end - start) / 2;
         }
-        return s;
+        return start;
     }
-    int binarySearch(vector<int>& nums, int target,int st,int en){
-        int mid = st+(en-st)/2;
-        while(st<=en){
-            if(nums[mid] == target)
+
+    // Binary search function to find the target element in the array
+    int binarySearch(vector<int>& nums, int target, int start, int end) {
+        int mid = start + (end - start) / 2;
+
+        while (start <= end) {
+            if (nums[mid] == target)
                 return mid;
-            else if(target>nums[mid])
-                st=mid+1;
+            else if (target > nums[mid])
+                start = mid + 1;
             else
-                en=mid-1;
-            mid = st+(en-st)/2;
+                end = mid - 1;
+
+            mid = start + (end - start) / 2;
         }
         return -1;
     }
 
 public:
     int search(vector<int>& nums, int target) {
-        int s=0,e=nums.size()-1;
+        int start = 0;
+        int end = nums.size() - 1;
+
+        // Find the pivot element (smallest element) in the rotated sorted array
         int pivot = findPivot(nums);
-        if(target >= nums[pivot] && target <= nums[e])
-            return binarySearch(nums,target,pivot,e);
+
+        // If the target lies between pivot and last element, perform binary search on that segment
+        if (target >= nums[pivot] && target <= nums[end])
+            return binarySearch(nums, target, pivot, end);
+        // Otherwise, perform binary search on the segment before pivot
         else
-            return binarySearch(nums,target,0,pivot-1);
+            return binarySearch(nums, target, start, pivot - 1);
     }
 };
+
 
 // Method 2
 // Given Array must be sorted on either side of the nums[mid], therefore we are checking for both the cases where it is
@@ -50,27 +63,40 @@ public:
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-     int n = nums.size();
-      int left = 0;
-      int right = n-1;
-      int mid= left + (right - left) / 2;
-      while(left <= right){
-        if(nums[mid] == target)
-         return mid;
-        if(nums[mid] >= nums[left]) {
-          if(target >= nums[left] && target <= nums[mid])
-            {
-                right = mid - 1;
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+        int mid = left + (right - left) / 2;
+
+        // Binary search loop
+        while (left <= right) {
+            // If the target is found at mid, return the index
+            if (nums[mid] == target)
+                return mid;
+
+            // Check if the left segment is sorted
+            if (nums[mid] >= nums[left]) {
+                // Check if the target lies within the left segment
+                if (target >= nums[left] && target <= nums[mid]) {
+                    right = mid - 1; // Update the right pointer to search in the left segment
+                } else {
+                    left = mid + 1; // Update the left pointer to search in the right segment
+                }
+            } 
+            // If the right segment is sorted
+            else {
+                // Check if the target lies within the right segment
+                if (target >= nums[mid] && target <= nums[right]) {
+                    left = mid + 1; // Update the left pointer to search in the right segment
+                } else {
+                    right = mid - 1; // Update the right pointer to search in the left segment
+                }
             }
-           else left = mid + 1;
-        } 
-        else {
-          if(target >= nums[mid] && target <= nums[right]) 
-            left = mid + 1;
-          else right = mid - 1;
+            // Calculate the new mid after updating the pointers
+            mid = left + (right - left) / 2;
         }
-          mid = left + (right - left) / 2;
-      }
-      return -1;
+
+        return -1; // If target is not found, return -1
     }
 };
+
