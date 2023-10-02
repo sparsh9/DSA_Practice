afa9c77fc9265f9ebf94e66d79a12e42b82cfca7
Prat2030 <pratyushjindal2003@gmail.com>
2023-10-02 21:56:51 +0530

    commit

--- 450_DSA_Sheet/Dynamic Programming/edit_distance.cpp
+++ 450_DSA_Sheet/Dynamic Programming/edit_distance.cpp
@@ -98,6 +98,7 @@ public:
         else{
             if(dp[i][j]!=-1)
                 return dp[i][j];
+            
             int text_insert = 1+helper(i,j-1,word1,word2,dp);
             int text_delete = 1+helper(i-1,j,word1,word2,dp);
             int text_replace = 1+helper(i-1,j-1,word1,word2,dp);
@@ -133,9 +134,11 @@ public:
         // now we will fill the dp array
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                // if it matches then we will move to the next character
+                // it is 1 based indexing for dp vector, but 0 based for the char string for both wordss, 
+                // therefore we check i-1,j-1 for words and update corresponding i,j values for dp vector array
                 if(word1[i-1]==word2[j-1])
                     dp[i][j]=dp[i-1][j-1];
+
                 // if it does not matches, then we will take the min of the 3 possibilities
                 // 1. we will do insertion and assume that we have inserted the character before in word1 and therefore we will move to the next character of text2
                 // 2. we will delete the character and assume that we have deleted the character from word1 and therefore we will move to the next character of text1
@@ -144,7 +147,6 @@ public:
                     dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));        
             }
         }
-
         return dp[n][m];
     }
 };
